232b3cfde5d2e5640455cd50417b24f7529670a4
dinnertime <dinnertime.dev@gmail.com>
2022-12-29 16:12:10 +0900

    2차

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const setObserver = (target, cb, option = { threshold: 1.0 }) => {
+  const observer = new IntersectionObserver(cb, option);
+  observer.observe(target);
+};
+
 const setSwiper = (selector) => {
   new Swiper(selector, {
     loop: true,
@@ -17,21 +22,46 @@ const setSwiper = (selector) => {
   });
 };
 
-const setFadeAnimation = (target) => {
+const setFadeAnimation = (target, count = 3, timeout = 3000) => {
   const imgs = target.querySelectorAll('img');
   let i = 0;
   setInterval(() => {
     for (const img of imgs) {
       img.classList.remove('show');
     }
-    imgs[i % 3].classList.add('show');
+    imgs[i % count].classList.add('show');
     i++;
-  }, 3000);
+  }, timeout);
 };
 
 const App = () => {
-  const bookClub = document.querySelector('[data-hb-target="bookClub"]');
-  setFadeAnimation(bookClub);
+  // const bookClub = document.querySelector('[data-hb-target="bookClub"]');
+  const banner = document.querySelector('[data-hb-target="banner"]');
+  // setFadeAnimation(bookClub);
+  setFadeAnimation(banner, 2, 3000);
   setSwiper('.hb__content-4-imgs');
+  const content2Texts = document.querySelectorAll('.hb__content-1-desc > span');
+  for (const text of content2Texts) {
+    setObserver(text, (entries) => {
+      for (const entry of entries) {
+        if (!entry.isIntersecting) return;
+        entry.target.classList.add('show');
+      }
+    });
+  }
+
+  const content2Items = document.querySelectorAll('.hb__content-2-item');
+  for (const item of content2Items) {
+    setObserver(
+      item,
+      (entries) => {
+        for (const entry of entries) {
+          if (!entry.isIntersecting) return;
+          entry.target.classList.add('show');
+        }
+      },
+      { threshold: 0.5 },
+    );
+  }
 };
 document.addEventListener('DOMContentLoaded', App);
